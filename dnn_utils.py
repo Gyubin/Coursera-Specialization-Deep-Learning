220111420f00568b12ebeffbff99aeb26092350e
Gyubin Son <geubin0414@gmail.com>
2017-09-15 21:20:30 -1100

    Add more utils

--- dnn_utils.py
+++ dnn_utils.py
@@ -434,3 +434,106 @@ def print_mislabeled_images(classes, X, y, p):
         plt.imshow(X[:,index].reshape(64,64,3), interpolation='nearest')
         plt.axis('off')
         plt.title("Prediction: " + classes[int(p[0,index])].decode("utf-8") + " \n Class: " + classes[y[0,index]].decode("utf-8"))
+
+def image2vector(image):
+    """
+    Argument:
+    image -- a numpy array of shape (length, height, depth)
+    
+    Returns:
+    v -- a vector of shape (length*height*depth, 1)
+    """
+    
+    ### START CODE HERE ### (≈ 1 line of code)
+    v = image.reshape(-1, 1)
+    ### END CODE HERE ###
+    
+    return v
+
+# GRADED FUNCTION: normalizeRows
+
+def normalizeRows(x):
+    """
+    Implement a function that normalizes each row of the matrix x (to have unit length).
+
+    Argument:
+    x -- A numpy matrix of shape (n, m)
+
+    Returns:
+    x -- The normalized (by row) numpy matrix. You are allowed to modify x.
+    """
+
+    ### START CODE HERE ### (≈ 2 lines of code)
+    # Compute x_norm as the norm 2 of x. Use np.linalg.norm(..., ord = 2, axis = ..., keepdims = True)
+    x_norm = np.linalg.norm(x, ord=2, axis=1, keepdims=True)
+
+    # Divide x by its norm.
+    x = x / x_norm
+    ### END CODE HERE ###
+
+    return x
+
+# GRADED FUNCTION: softmax
+
+def softmax(x):
+    """Calculates the softmax for each row of the input x.
+
+    Your code should work for a row vector and also for matrices of shape (n, m).
+
+    Argument:
+    x -- A numpy matrix of shape (n,m)
+
+    Returns:
+    s -- A numpy matrix equal to the softmax of x, of shape (n,m)
+    """
+
+    ### START CODE HERE ### (≈ 3 lines of code)
+    # Apply exp() element-wise to x. Use np.exp(...).
+    x_exp = np.exp(x)
+
+    # Create a vector x_sum that sums each row of x_exp. Use np.sum(..., axis = 1, keepdims = True).
+    x_sum = np.sum(x_exp, axis=1, keepdims=True)
+
+    # Compute softmax(x) by dividing x_exp by x_sum. It should automatically use numpy broadcasting.
+    s = x_exp / x_sum
+
+    ### END CODE HERE ###
+
+    return s
+
+# GRADED FUNCTION: L1
+
+def L1(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+
+    Returns:
+    loss -- the value of the L1 loss function defined above
+    """
+
+    ### START CODE HERE ### (≈ 1 line of code)
+    loss = np.sum(np.abs(y - yhat))
+    ### END CODE HERE ###
+
+    return loss
+
+# GRADED FUNCTION: L2
+
+def L2(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+
+    Returns:
+    loss -- the value of the L2 loss function defined above
+    """
+
+    ### START CODE HERE ### (≈ 1 line of code)
+    loss = np.dot(y-yhat, y-yhat)
+    ### END CODE HERE ###
+
+    return loss
+
